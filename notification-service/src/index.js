d36be1e3530ca29c431b538b74959740832aa74a
githubwebdev <fakhrulazmi01@student.ciputra.ac.id>
2025-07-11 01:40:25 +0700

    init

--- notification-service/src/index.js
+++ notification-service/src/index.js
@@ -8,6 +8,15 @@ wss.on('connection', function connection(ws) {
   // Kirim pesan awal
   ws.send(JSON.stringify({ message: 'Terhubung ke Notification Service 🚀' }));
 
+  // 🌐 Inter-service call ke user-service
+axios.get('http://user-service:3001/users')
+  .then(res => {
+    console.log('User service response:', res.data);
+  })
+  .catch(err => {
+    console.error('Error accessing user-service:', err.message);
+  });
+  
   // Simulasi notifikasi tiap 10 detik
   const interval = setInterval(() => {
     ws.send(JSON.stringify({ message: '🔔 Notifikasi baru: Pesanan Anda sedang dikemas!' }));
